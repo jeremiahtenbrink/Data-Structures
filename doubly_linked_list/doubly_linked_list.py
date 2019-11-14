50f24b91569ce982e454092666f30a36cf7893d2
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-11-13 22:08:27 -0700

    Finished double linked list.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -87,7 +87,7 @@ class DoublyLinkedList:
             self.tail = new_node
         else:
             new_node.prev = self.tail
-            self.tail.prev = new_node
+            self.tail.next = new_node
             self.tail = new_node
 
     """Removes the List's current tail node, making the 
@@ -103,13 +103,23 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        else:
+            value = node.value
+            self.delete(node)
+            self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        else:
+            value = node.value
+            self.delete(node)
+            self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
