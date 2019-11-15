84908d197c1d00653b50e286f9226aac33d53500
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-11-14 21:44:36 -0700

    Finished max heap. Could not get generic heap to work.

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,22 +1,60 @@
 class Heap:
-    def __init__(self, comparator):
+    def __init__(self, comparator = lambda x, y: x < y):
         self.storage = []
+        self.priority = []
         self.comparator = comparator
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self.priority.append(len(self.storage) - 1)
+        self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        pass
+        self.storage[0], self.storage[len(self.storage) - 1] = \
+            self.storage[len(self.storage) - 1], self.storage[0]
+        self.priority[0], self.priority[len(self.storage) - 1] = \
+            self.priority[len(self.storage) - 1], self.priority[0]
+        self._sift_down(0)
+        self.priority.pop()
+        return self.storage.pop()
 
     def get_priority(self):
-        pass
+        return self.priority[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        parent_index = (index - 1) // 2
+
+        larger = self.comparator(self.storage[index], self.storage[parent_index])
+
+        if index > 0 and larger:
+            self.storage[index], self.storage[parent_index] = \
+                self.storage[parent_index], self.storage[index]
+            self.priority[index], self.priority[parent_index] = \
+                self.priority[parent_index], self.priority[index]
+            self._bubble_up(parent_index)
 
     def _sift_down(self, index):
-        pass
+        index_left = index * 2 + 1
+        index_right = index * 2 + 2
+
+        index_largest = index
+
+        if index_left < len(self.storage) - 1:
+            compar = self.comparator(self.storage[index], self.storage[index_left])
+            if (compar):
+                index_largest = index_left
+
+        if index_right < len(self.storage) - 1:
+            compar = self.comparator(self.storage[index_largest], self.storage[index_right])
+            if compar:
+                index_largest = index_right
+
+        if index != index_largest:
+            self.storage[index_largest], self.storage[index] = \
+                self.storage[index], self.storage[index_largest]
+            self.priority[index_largest], self.priority[index] = \
+                self.priority[index], self.priority[index_largest]
+            self._sift_down(index_largest)
--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,51 @@
-class Heap:
+class Heap():
     def __init__(self):
+        self.storage = []
+        self.size = 0
         self.storage = []
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
+        self._bubble_up(self.size - 1)
 
     def delete(self):
-        pass
+        max = self.storage[0]
+        self.storage[self.size - 1], self.storage[0] = self.storage[0], self.storage[self.size - 1]
+        self.size -= 1
+        self.storage.pop()
+        self._sift_down(0)
+        return max
 
     def get_max(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return self.size
 
     def _bubble_up(self, index):
-        pass
+        parent_index = (index - 1) // 2
+
+        if index > 0 and self.storage[index] > self.storage[parent_index]:
+            self.storage[index], self.storage[parent_index] = self.storage[
+                                                            parent_index], \
+                                                        self.storage[index]
+            self._bubble_up(parent_index)
 
     def _sift_down(self, index):
-        pass
+        index_left = 2 * index + 1
+        index_right = 2 * index + 2
+
+        index_largest = index
+
+        if index_left < self.size and self.storage[index_left] > self.storage[index]:
+            index_largest = index_left
+        if index_right < self.size and self.storage[index_right] > self.storage[
+            index_largest]:
+            index_largest = index_right
+
+        if index != index_largest:
+            self.storage[index_largest], self.storage[index] = self.storage[index], \
+                                                         self.storage[
+                                                             index_largest]
+            self._sift_down(index_largest)
